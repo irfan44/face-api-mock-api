026a9b339bfcb799c6a3bffbb14b1f24e53a9a25
irfan44 <irfannmuhajir@gmail.com>
2023-06-24 09:43:30 +0700

    fix: change port

--- index.js
+++ index.js
@@ -1,11 +1,10 @@
 const express = require("express");
 const app = express();
 const faceApiMock = require("./controllers/face-api-mock");
-const port = 3000;
 
 app.use(express.json());
 app.use("/", faceApiMock);
 
-app.listen(port, () => {
-  console.log(`app running at http://localhost:${port}`);
+app.listen(process.env.PORT, () => {
+  console.log(`app running at http://localhost:${process.env.PORT}`);
 });
